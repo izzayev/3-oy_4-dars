89bfa554d216f9bb1cc6efff1e7a287585d7e126
izzayev <alisherizzayev2003@gmail.com>
2022-07-07 18:12:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var promtInput = prompt("Ismingiz");
+var promtInput = prompt("Familyangiz");
+var promtInput = prompt("Otaigizning ismi");
+var promtInput = prompt("Tug'ilgan sanangiz");
+var promtInput = prompt("Tug'ilgan joyingiz");
+var promtInput = prompt("Vazningiz");
+var promtInput = prompt("Malumotingiz");
+
+alert(`Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${promtInput}, 
+Result: ${"muvafaqiyatli o'tkazildi"},`)
+
+console.log(`Result: ${promtInput}, Result: ${promtInput}, Result: ${promtInput}, Result: ${promtInput}, Result: ${promtInput}, Result: ${promtInput}, Result: ${promtInput}, Result: ${"muvafaqiyatli o'tkazildi"},`);
